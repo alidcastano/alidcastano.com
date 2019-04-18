d44d16c79945b498cdc9386669f5256a339d724f
Alid Castano <alidcastano@Alids-MacBook-Air.local>
2019-04-18 09:36:46 -0500

    added twitter to contact

--- pages/index/contact.vue
+++ pages/index/contact.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
 PreviewContainer
   h2 Let's chat
-  p You can reach me at alidcastano@gmail.com.
+  p You can reach me at alidcastano(at)gmail(dot)com.
+  p For updates, follow my Twitter <a href="https://mobile.twitter.com/alidcastano">@alidcastano</a>.
 </template>
 
 <script>
