b82955aaa9726dd65c7ac075ecbb9b34007a74ea
Alid Castano <alidcastano@Alids-MacBook-Air.local>
2019-04-19 16:20:41 -0500

    temporarily removed philo from about (until i write about it more in depthlater)

--- pages/index/about.vue
+++ pages/index/about.vue
@@ -4,7 +4,6 @@ PreviewContainer
   p I care deely about how we learn and share information online.
   p My <a href="https://alidcastano.com/2016/be-uniquely-valuable" target="_blank">three skills</a> are coding, public speaking, and writing.
   p I'm fascinated by startups, fitness, usable psychology, and conscious growth. Oh, and <a href="http://studbits.com" target="_blank">note-taking</a>!
-  p My life philosophy is a mix between Stoicism and Objectivism.
   p The best advice I've received is <a href="https://alidcastano.com/2016/stay-upwind" target="_blank">"Stay Upwind"</a>.
   p I was born in Colombia, raised in Miami, educated in Philly, went monk mode in Minnapolis,
     |  and now looking to reside in NYC.
